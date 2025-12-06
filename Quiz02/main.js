8e75046926218d4d63a2f06524b430dd87035f33
Kerolos Amiel <kerolosamiel4@gmail.com>
2025-12-06 14:22:43 +0200

    refactor: Quiz02

---
+++ Quiz02/main.js
@@ -0,0 +1,30 @@
+console.log("----------1----------");
+// Don't use any numbers
+
+let myFriends = ["Ahmed", "Elham", "Osama", "Gamal"];
+let num = 3;
+
+// Method 1
+console.log("Your Code Here"); // ["Ahmed", "Elham", "Osama"];
+
+// Method 2
+console.log("Your Code Here"); // ["Ahmed", "Elham", "Osama"];
+
+console.log("----------2----------");
+
+//Don't use slice method
+let friends = ["Ahmed", "Eman", "Osama", "Gamal"];
+
+// Write Your Code Here
+
+console.log(friends); // ["Eman", "Osama"]
+
+console.log("----------3----------");
+// Don't use any number
+let arrOne = ["C", "D", "X"];
+let arrTwo = ["A", "B", "Z"];
+let finalArr = [];
+
+// Write One Single Line Of Code
+
+console.log(finalArr); // ["Z", "X", "D", "C", "B", "A"]
